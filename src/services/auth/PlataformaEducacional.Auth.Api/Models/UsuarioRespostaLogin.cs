278848b5916ee49424afbda918392a4eb832771e
Cleber Movio <crmmvio@gmail.com>
2026-02-08 23:50:00 -0300

    feat(auth): tornar AccessToken não nulo em UsuarioRespostaLogin
    * Altera a propriedade AccessToken de string? para string.
    * Define valor padrão como string.Empty, evitando valores nulos.
    * Garante maior segurança e previsibilidade ao acessar o token.

--- src/services/auth/PlataformaEducacional.Auth.Api/Models/UsuarioRespostaLogin.cs
+++ src/services/auth/PlataformaEducacional.Auth.Api/Models/UsuarioRespostaLogin.cs
@@ -2,7 +2,7 @@
 
 public class UsuarioRespostaLogin
 {
-    public string? AccessToken { get; set; }
+    public string AccessToken { get; set; } = string.Empty;
     public double ExpiresIn { get; set; }
     public UsuarioToken? UsuarioToken { get; set; }
 }
