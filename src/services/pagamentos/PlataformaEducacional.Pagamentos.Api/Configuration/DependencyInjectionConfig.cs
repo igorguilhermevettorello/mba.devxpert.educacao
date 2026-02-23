088f8d7c13263e90cae0795472bd38eec40ee6cb
= <=>
2026-02-23 20:50:31 -0300

    Criação das funcionalidades iniciais da api de pagamentos

---
+++ src/services/pagamentos/PlataformaEducacional.Pagamentos.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using PlataformaEducacional.Pagamentos.Api.Data;
+using PlataformaEducacional.Pagamentos.Api.Data.Repository;
+using PlataformaEducacional.Pagamentos.Api.Facade;
+using PlataformaEducacional.Pagamentos.Api.Models;
+using PlataformaEducacional.Pagamentos.Api.Services;
+using PlataformaEducacional.WebApi.Core.User;
+
+namespace PlataformaEducacional.Pagamentos.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+
+            services.AddScoped<IPagamentoService, PagamentoService>();
+            services.AddScoped<IPagamentoFacade, PagamentoCartaoCreditoFacade>();
+
+            services.AddScoped<IPagamentoRepository, PagamentoRepository>();
+            services.AddScoped<PagamentosContext>();
+        }
+    }
+}
---
+++ src/services/pagamentos/PlataformaEducacional.Pagamentos.EducaPag/EducaPagService.cs
@@ -0,0 +1,14 @@
+﻿namespace PlataformaEducacional.Pagamentos.EducaPag
+{
+    public class EducaPagService
+    {
+        public readonly string ApiKey;
+        public readonly string EncryptionKey;
+
+        public EducaPagService(string apiKey, string encryptionKey)
+        {
+            ApiKey = apiKey;
+            EncryptionKey = encryptionKey;
+        }
+    }
+}
