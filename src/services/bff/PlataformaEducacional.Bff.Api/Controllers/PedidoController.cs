23455312c25d87c2e92018a67e4621b1e870e523
Cleber Movio <crmmvio@gmail.com>
2026-03-20 23:39:26 -0300

    feat(bff): reorganiza estrutura e adiciona PedidoController
    * Reorganiza o projeto BFF sob as pastas "ApiGateways/Compras"
    * Adiciona pastas "Configurations", "Models", "Extensions" e "Services" ao projeto BFF
    * Remove exemplos padrão WeatherForecast do BFF
    * Adiciona o controller "PedidoController" com action Index
    * Atualiza MediatR para 14.1.0 em Core e adiciona em Conteudo.Api
    * Atualiza AutoMapper para 16.1.1 em Conteudo.Api
    * Atualiza GUIDs e aninhamento de projetos na solução

---
+++ src/services/bff/PlataformaEducacional.Bff.Api/Controllers/PedidoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlataformaEducacional.Bff.Api.Controllers;
+
+public class PedidoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
