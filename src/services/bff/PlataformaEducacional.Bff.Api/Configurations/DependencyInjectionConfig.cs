8e9de7fbe585832c9d9e197452cdd3e597854f89
Cleber Movio <crmmvio@gmail.com>
2026-03-30 20:20:26 -0300

    feat(api): adicionar CompraController e configs iniciais
    * Remove PedidoController e sua action Index
    * Adiciona CompraController herdando de MainController com action Index
    * Cria DependencyInjectionConfig para configuração futura de DI
    * Remove referência à pasta Configurations no csproj
    * Adiciona URLs de serviços externos (Catalogo, Pagamento, Aluno) no appsettings.Development.json

---
+++ src/services/bff/PlataformaEducacional.Bff.Api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace PlataformaEducacional.Bff.Api.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+
+    }
+}
--- src/services/bff/PlataformaEducacional.Bff.Api/Controllers/CompraController.cs
+++ src/services/bff/PlataformaEducacional.Bff.Api/Controllers/CompraController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PlataformaEducacional.WebApi.Core.Controllers;
 
 namespace PlataformaEducacional.Bff.Api.Controllers;
 
-public class PedidoController : Controller
-{
+public class CompraController : MainController
+{ 
     public IActionResult Index()
     {
         return View();
