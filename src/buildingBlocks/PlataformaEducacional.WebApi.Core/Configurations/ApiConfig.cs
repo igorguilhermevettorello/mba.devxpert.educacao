9934cb2cf6c2f2b41f175886b70a2822fe7e2740
Cleber Movio <crmmvio@gmail.com>
2026-06-30 22:06:39 -0300

    feat(nginx): add new NGINX setup for reverse proxy
    * Adicionar arquivos de configuração `nginx.prod.conf` e `nginx.test.conf` ao projeto, removendo o antigo `nginx.conf`.
    * Atualizar `docker-common-resources.yml` para incluir o novo serviço `mba-devexpert-educacao-server` com NGINX, expondo as portas 80 e 443.
    * Comentar mapeamentos de portas em `docker-compose-local.yml` para serviços como `api-auth`, `api-aluno`, `api-conteudo`, `api-pagamentos` e `api-bff`, indicando uso de proxy reverso.
    * Atualizar `dockerfile` para usar `nginx.test.conf` em vez de `nginx.conf`.
    * Adicionar parâmetro `setProxyReverse` em `ApiConfig.cs` para configurar cabeçalhos encaminhados quando em proxy reverso.
    * Modificar `Program.cs` para incluir `setProxyReverse` na chamada de `AddApiConfiguration`, baseado no ambiente "Docker".
    * Melhorar o manuseio de requisições HTTP e otimizar o processo de implantação usando NGINX como proxy reverso.

--- src/buildingBlocks/PlataformaEducacional.WebApi.Core/Configurations/ApiConfig.cs
+++ src/buildingBlocks/PlataformaEducacional.WebApi.Core/Configurations/ApiConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PlataformaEducacional.WebApi.Core.Identity;
@@ -9,7 +10,7 @@ namespace PlataformaEducacional.WebApi.Core.Configurations;
 
 public static class ApiConfig
 {
-    public static IServiceCollection AddApiConfiguration(this IServiceCollection services, string apiName, bool authenticationRequired = true)
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services, string apiName, bool authenticationRequired = true, bool setProxyReverse = false)
     {
         services.AddSwagger(apiName, authenticationRequired);
 
@@ -22,6 +23,12 @@ public static class ApiConfig
                 {
                     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
                 });
+
+        if (setProxyReverse)
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
 
         services.AddCors(options =>
         {
@@ -36,8 +43,11 @@ public static class ApiConfig
         return services;
     }
 
-    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env, bool setProxyReverse = false)
     {
+        if (setProxyReverse)
+            app.UseForwardedHeaders();
+
         if (env.IsDevelopment() || env.IsEnvironment("Docker"))
         {
             app.UseDeveloperExceptionPage();
